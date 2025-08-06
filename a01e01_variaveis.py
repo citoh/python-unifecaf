40a945cba031efda488b5c4533fb947af81aa7f0
Francisco Citó <francisco.cito@waveweb.pt>
2025-08-06 16:43:50 -0300

    Add comments

--- a01e01_variaveis.py
+++ a01e01_variaveis.py
@@ -1,7 +1,9 @@
-import sys
+import sys # sys é um biblioteca naitva do Python para processos junto ao SO
 
-output = "ALOCAÇÃO BASE EM BYTES ---------------------------------\n"
+#Crianco uma variável de saída
+output = "ALOCAÇÃO BASE EM BYTES ---------------------------------\n" # \n é quebra de linha
 
+# types é do tipo dict e está armazenando valores dos tipos mais comuns de variáveis
 types = {
     "int": 0,
     "float": 0.0,
@@ -14,12 +16,14 @@ types = {
     "NoneType": None,
 }
 
+# lado que imprime o quanto cada valor dentro de types está ocupando da memória RAM
 for name, value in types.items():
     output += f"{name:<8} ->  {sys.getsizeof(value)} bytes\n"
 
 output += "\n"
 output += "TESTE A SUA VARIÁVEL -----------------------------------\n"
 
+# coloque o valor da sua variável
 x = 1
 output += f"Valor: {x}\n"
 output += f"Tipo: {type(x).__name__}\n"
@@ -29,6 +33,7 @@ output += "\n"
 # Imprime na tela o que temos na memória RAM (variável output)
 print(output)
 
-# Salvando em arquivo o que temos na memória RAM (variável output)
-with open("a1_variaveis/a1_variaveis_dados.txt", "w", encoding="utf-8") as file:
-    file.write(output)
+
+# EXTRA - Salvando em arquivo o que temos na memória RAM (variável output)
+# with open("variaveis_dados.txt", "w", encoding="utf-8") as file:
+#    file.write(output)
