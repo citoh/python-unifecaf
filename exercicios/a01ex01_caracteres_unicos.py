2d3dd96a3a8ca2feec6ace4d7cc276ed95df81ad
Francisco Citó <francisco.cito@waveweb.pt>
2025-08-07 15:45:24 -0300

    Add exercises class 1

---
+++ exercicios/a01ex01_caracteres_unicos.py
@@ -0,0 +1,6 @@
+# Leia todos os caracteres de uma string e retorne todos os 
+# caracteres que não possuírem par. Por exemplo:
+#     aabcc  => b
+#     aabbccaafbbbaaacaa  => fc
+#     aabbcc => None
+
