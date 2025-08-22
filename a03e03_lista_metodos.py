818d153e6b63e4dd698ceccb8044ab4e9113d33c
Francisco Citó <francisco.cito@waveweb.pt>
2025-08-22 18:41:28 -0300

    Code review

--- a03e03_lista_metodos.py
+++ a03e03_lista_metodos.py
@@ -1,9 +1,16 @@
-lojas = ["Magazine Luiza", "Casas Bahia", "Americanas", "Renner", "Havan"]
+carros = ["Fiat", "Chevrolet", "Volkswagen", "Ford", "Honda", "Toyota"]
 
-lojas.sort() # ordena a lista em ordem alfabética crescente
-lojas.reverse() # inverte a ordem atual
+carros.append("Audi") # adiciona UM item ao final
 
-sorted(lojas, key=len) # ordena pelo tamanho do nome da loja
-sorted(lojas, reverse=True) # ordena a lista em ordem alfabética decrescente
+carros.extend(["BMW", "Mercedes"]) # adiciona VÁRIOS itens de outra lista
+carros.insert(2, "Ferrari") # insere item em posição específica
+carros.remove("Ford") # remove a PRIMEIRA ocorrência de um valor
 
-sorted(lojas, key=lambda x: x.split()[-1]) # Ordena pelo último nome
+carros.pop() # remove o último item e retorna o seu valor
+carros.pop(1)  # também pode remover um item de posição específica
+
+carros.clear() # clear -> remove TODOS os elementos
+
+carros.sort() # sort -> ordena a lista em ordem alfabética
+carros.sort(reverse=True) # com reverse=True a lista é ordenada de forma decrescente
+carros.reverse() # reverse -> inverte a ordem atual
--- a03e04_lista_ordenacao.py
+++ a03e04_lista_ordenacao.py
@@ -1,16 +1,9 @@
-carros = ["Fiat", "Chevrolet", "Volkswagen", "Ford", "Honda", "Toyota"]
+lojas = ["Magazine Luiza", "Casas Bahia", "Americanas", "Renner", "Havan"]
 
-carros.append("Audi") # adiciona UM item ao final
+lojas.sort() # ordena a lista em ordem alfabética crescente
+lojas.reverse() # inverte a ordem atual
 
-carros.extend(["BMW", "Mercedes"]) # adiciona VÁRIOS itens de outra lista
-carros.insert(2, "Ferrari") # insere item em posição específica
-carros.remove("Ford") # remove a PRIMEIRA ocorrência de um valor
+sorted(lojas, key=len) # ordena pelo tamanho do nome da loja
+sorted(lojas, reverse=True) # ordena a lista em ordem alfabética decrescente
 
-carros.pop() # remove o último item e retorna o seu valor
-carros.pop(1)  # também pode remover um item de posição específica
-
-carros.clear() # clear -> remove TODOS os elementos
-
-carros.sort() # sort -> ordena a lista em ordem alfabética
-carros.sort(reverse=True) # com reverse=True a lista é ordenada de forma decrescente
-carros.reverse() # reverse -> inverte a ordem atual
+sorted(lojas, key=lambda x: x.split()[-1]) # Ordena pelo último nome
